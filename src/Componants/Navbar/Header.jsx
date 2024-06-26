18c7edb8b4f4b0f5e300287451f13ac880811d64
Amaan Sodagar <sodagaramaan786@gmail.com>
2024-06-26 17:00:23 +0530

    update

--- src/Componants/Navbar/Header.jsx
+++ src/Componants/Navbar/Header.jsx
@@ -1,73 +1,103 @@
-import React, { useState, useEffect } from 'react';
-import "./Header.scss";
+import React, { useState } from 'react';
+import './Header.scss';
 import { Link, useNavigate } from 'react-router-dom';
-import { AiOutlineHome } from "react-icons/ai";
-// import { BsPerson, BsCodeSlash } from "react-icons/bs";
-// import { CgFileDocument } from "react-icons/cg";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { IoCloseOutline } from "react-icons/io5";
-import logo from '../../Images/Logo/NWDS-Logo-CDR.png'
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { IoCloseOutline } from 'react-icons/io5';
+import logo from '../../Images/Logo/NWDS-Logo-CDR.png';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [activestate, setActivestate] = useState('');
-  const Navigate = useNavigate();
-
-
-
-
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const navigate = useNavigate();
 
   const showMenu = () => {
     setMenuOpen(!menuOpen);
-    const ham = document.getElementsByClassName("NavbarLinks")[0];
-    ham.classList.toggle("showNavbar");
-  }
+    const ham = document.getElementsByClassName('NavbarLinks')[0];
+    ham.classList.toggle('showNavbar');
+  };
 
   const hideMenu = (id) => {
     setMenuOpen(false);
-    const ham = document.getElementsByClassName("NavbarLinks")[0];
-    ham.classList.remove("showNavbar");
+    const ham = document.getElementsByClassName('NavbarLinks')[0];
+    ham.classList.remove('showNavbar');
     setActivestate(id);
+    setDropdownOpen(false); // Close the dropdown when an item is clicked
+  };
 
-  }
   const navigatetohome = (id) => {
-    Navigate('/')
+    navigate('/');
     setActivestate(id);
+  };
 
-  }
+  const toggleDropdown = (event) => {
+    event.stopPropagation(); // Prevent event bubbling to avoid closing the dropdown
+    setDropdownOpen(!dropdownOpen);
+    setActivestate('services');
+  };
 
   return (
-
-    <nav className='Navbar'>
-      <img onClick={() => navigatetohome('home')}
-
+    <nav className='Navbar' onClick={() => setDropdownOpen(false)}>
+      <img
+        onClick={() => navigatetohome('home')}
         className='Logo'
-        src={logo} alt="" srcset=""
+        src={logo}
+        alt=''
+        srcSet=''
       />
-      
       <div className='Hamburger' onClick={showMenu}>
         {menuOpen ? <IoCloseOutline /> : <RxHamburgerMenu />}
       </div>
-
-      <ul className='NavbarLinks'>
-        <li id='home' className={activestate === 'home' ? 'active' : ''} onClick={() => hideMenu('home')}>
-          <Link to="/">Home</Link>
+      <ul className='NavbarLinks' onClick={(e) => e.stopPropagation()}>
+        <li
+          id='home'
+          className={activestate === 'home' ? 'active' : ''}
+          onClick={() => hideMenu('home')}
+        >
+          <Link to='/'>Home</Link>
         </li>
-        <li id='about' className={activestate === 'about' ? 'active' : ''} onClick={() => hideMenu('about')}>
-          <Link to="/about"> About</Link>
+        <li
+          id='about'
+          className={activestate === 'about' ? 'active' : ''}
+          onClick={() => hideMenu('about')}
+        >
+          <Link to='/about'>About</Link>
         </li>
-        <li id='project' className={activestate === 'project' ? 'active' : ''} onClick={() => hideMenu('project')}>
-          <Link to="/service"> Services</Link>
+        <li
+          id='services'
+          className={activestate === 'services' ? 'active' : ''}
+          onClick={toggleDropdown}
+        >
+          <span>Services</span>
+          <ul className={`Dropdown ${dropdownOpen ? 'show' : ''}`}>
+            <li onClick={() => hideMenu('service1')}>
+              <Link to='/service1'>Hr Consultancy</Link>
+            </li>
+            <li onClick={() => hideMenu('service2')}>
+              <Link to='/service2'>Event Management</Link>
+            </li>
+            <li onClick={() => hideMenu('service3')}>
+              <Link to='/service3'>Insurance</Link>
+            </li>
+          </ul>
         </li>
-        <li id='career' className={activestate === 'career' ? 'active' : ''} onClick={() => hideMenu('career')}>
-          <Link to="/career"> Careers</Link>
+        <li
+          id='career'
+          className={activestate === 'career' ? 'active' : ''}
+          onClick={() => hideMenu('career')}
+        >
+          <Link to='/career'>Careers</Link>
         </li>
-        <li id='contact' className={activestate === 'contact' ? 'active' : ''} onClick={() => hideMenu('contact')}>
-          <Link to="/contact"> Contact us</Link>
+        <li
+          id='contact'
+          className={activestate === 'contact' ? 'active' : ''}
+          onClick={() => hideMenu('contact')}
+        >
+          <Link to='/contact'>Contact us</Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Header;
