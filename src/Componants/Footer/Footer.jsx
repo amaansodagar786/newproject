c5c8fc3d7621e264b5dff978fe3127302cda1094
Amaan Sodagar <sodagaramaan786@gmail.com>
2025-01-17 19:23:02 +0530

    comm

--- src/Componants/Footer/Footer.jsx
+++ src/Componants/Footer/Footer.jsx
@@ -8,12 +8,12 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-content">
         <p className='upperline'>NOAH Workforce Development System</p>
-        <p> <FaLocationDot /> &nbsp; Vasna, Vadodara, Gujarat, India</p>
-        <p><FaPhoneAlt /> &nbsp; +91 9510287060</p>
+        <p> <FaLocationDot /> &nbsp; Vadodara, Gujarat, India</p>
+        <p><FaPhoneAlt /> &nbsp; +91 1234567890</p>
         <p><FaEnvelope /> &nbsp; info@noahwds.com </p>
       </div>
       <div className="footer-bottom">
-        <p>&copy; 2024 Noah Workforce Development System - All Rights Reserved.</p>
+        <p>&copy; 2025 Noah Workforce Development System - All Rights Reserved.</p>
         <div className="developed-by">
           
           <a href="https://www.instagram.com/infinity.incorp/" target="_blank" rel="noopener noreferrer">
